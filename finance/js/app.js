2eef8ed569f37edd255737197a629863b0114c95
matheuszeuch <matheuszeuch@gmail.com>
2016-11-17 10:59:22 -0200

    Add files via upload

--- finance/js/app.js
+++ finance/js/app.js
@@ -5,8 +5,18 @@ window.debug = {};
 document.addEventListener('init', function(event) {
   //document.ontouchmove = function(e) {e.preventDefault()};
   //bouncefix.add('scrollfix');
-  $(document).on("touchmove", function(evt) { evt.preventDefault() });
-  $(document).on("touchmove", ".content", function(evt) { evt.stopPropagation() });
+  $(document).on("touchmove", function(evt) { evt.preventDefault(); });
+  $(document).on("touchmove", ".content", function(evt) {
+    var startY, startTopScroll;
+    var elem = this;
+    elem.addEventListener('touchstart', function(event){
+      startY = event.touches[0].pageY;
+      startTopScroll = elem.scrollTop;
+      if (startTopScroll <= 0) elem.scrollTop = 1;
+      if (startTopScroll + elem.offsetHeight >= elem.scrollHeight) elem.scrollTop = elem.scrollHeight - elem.offsetHeight - 1;
+    }, false);
+    evt.stopPropagation();
+  });
 
   debug = function(text) {
     $("#console").append("<p>" + text + "</p>");
